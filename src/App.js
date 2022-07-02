77c4db5d9f9c9e7ed2abcd66e4795fe277893b94
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-07-02 14:39:58 +0700

    Add: Home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Home } from './pages';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
