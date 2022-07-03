3a55f860adb3f957e8388f701c4a214587ef5a7f
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-07-03 16:47:06 +0700

    Add: SectionTitle

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,13 +1,12 @@
 import ExperienceGroup from "./ExperienceGroup";
 import experienceIcon from "../../assets/icons/experience-icon.png";
+import SectionTitle from "../SectionTitle";
 import './style.css';
 
 const Experience = () => {
     return (
         <div className="experience flex flex-col items-center justify-center text-center py-10">
-            <img src={experienceIcon} alt=" " className="w-16 mb-3" />
-            <h4 className="font-bold text-blue-600">Experience</h4>
-            <h2 className="font-bold text-2xl md:w-[500px] mt-3 mb-5">I have some experience in the field of web development</h2>
+            <SectionTitle icon={experienceIcon} smallTitle="Experience" bigTitle="I have some experience in the field of web development" />
             <ExperienceGroup />
         </div>
     )
---
+++ src/components/SectionTitle/index.js
@@ -0,0 +1,12 @@
+const SectionTitle = ({icon, smallTitle, bigTitle, description}) => {
+    return (
+        <div className="mb-5">
+            <img src={icon} alt=" " className="w-16 mb-3 mx-auto" />
+            <h4 className="font-bold text-blue-600">{smallTitle}</h4>
+            <h2 className="font-bold text-2xl md:w-[500px] my-3 mx-auto">{bigTitle}</h2>
+            <p className="max-w-[500px]">{description}</p>
+        </div>
+    )
+}
+
+export default SectionTitle;
