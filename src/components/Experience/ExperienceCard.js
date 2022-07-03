233912e33a95a179a808da6038dbc5572aae444e
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-07-03 16:23:56 +0700

    Add: ExperienceCard props

--- src/components/Experience/ExperienceCard.js
+++ src/components/Experience/ExperienceCard.js
@@ -1,10 +1,10 @@
-const ExperienceCard = () => {
+const ExperienceCard = ({ role, company, period, description }) => {
     return (
         <div className="experience__card text-left p-5 mb-2 md:m-3 shadow-md rounded border-t-[6px] border-blue-600 md:border-none">
-            <h3 className="font-bold">Fullstack Web Developer</h3>
-            <p>PT Solusi Intek Indonesia</p>
-            <p className="text-gray-400 transition-all text-xs mb-2">April 2021 - October 2021</p>
-            <p className="text-gray-500 text-sm transition-all anima">Collaborate in helping to complete assigned projects, Develop village information website and salary management website</p>
+            <h3 className="font-bold">{ role }</h3>
+            <p>{ company }</p>
+            <p className="text-gray-400 transition-all text-xs mb-2">{period}</p>
+            <p className="text-gray-500 text-sm transition-all anima">{description}</p>
         </div>
     )
 }
--- src/components/Experience/ExperienceGroup.js
+++ src/components/Experience/ExperienceGroup.js
@@ -3,9 +3,9 @@ import ExperienceCard from "./ExperienceCard";
 const ExperienceGroup = () => {
     return (
         <div className="experience__group flex flex-wrap justify-center">
-            <ExperienceCard />
-            <ExperienceCard />
-            <ExperienceCard />
+            <ExperienceCard role="Fulstack Web Developer" company="PT Solusi Intek Indonesia" period="April 2021 - October 2021" description="Collaborate in helping to complete assigned projects, Develop village information website and salary management website" />
+            <ExperienceCard role="Fulstack Web Developer" company="PT Solusi Intek Indonesia" period="April 2021 - October 2021" description="Collaborate in helping to complete assigned projects, Develop village information website and salary management website" />
+            <ExperienceCard role="Fulstack Web Developer" company="PT Solusi Intek Indonesia" period="April 2021 - October 2021" description="Collaborate in helping to complete assigned projects, Develop village information website and salary management website" />
         </div>
     )
 }
